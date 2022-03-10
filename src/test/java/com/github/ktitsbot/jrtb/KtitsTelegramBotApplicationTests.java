4ada7f14bb219870f842d34367f55b228ad7db94
FanTei <passmirgeb@gmail.com>
2022-03-10 21:39:33 +0300

    Main: fix problems

---
+++ src/test/java/com/github/ktitsbot/jrtb/KtitsTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ktitsbot.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtitsTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
